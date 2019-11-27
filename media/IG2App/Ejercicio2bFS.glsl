dcf592b1f3ee3209826b661031490cbe2a4a40fa
Alvaro <alsanz16@ucm.es>
2019-11-27 12:10:50 +0100

    Luz hecha YUJIIIIIUUU

--- media/IG2App/Ejercicio2bFS.glsl
+++ media/IG2App/Ejercicio2bFS.glsl
@@ -3,8 +3,6 @@ uniform sampler2D textura0; // tipo sampler2D para texturas 2D
 uniform sampler2D textura1;
 uniform vec4 InColor;
 uniform vec4 OutColor;
-uniform float FB; // blending factor
-uniform float intLuzAmb; // luz ambiente
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor; // out del fragment shader
 
@@ -23,27 +21,24 @@ void main() {
 	vec3 color0 = vec3(texture(textura0, vUv0)); // acceso a texel
 	vec3 color1 = vec3(texture(textura1, vUv0)); // configuración
 	
-	//diffuse
-	vec3 norm = normalize(vWnormal);
-	vec3 inverseNorm = normalize(1.0/vWnormal);
-	vec3 lightDir = normalize(lightPosition-vWvertex);
-	float diff = max(dot(norm,lightDir),0.0);
-	float inverseDiff = max(dot(inverseNorm,lightDir),0.0);
-	vec3 diffuse = diff*lightDiffuse*materialDiffuse;
-	vec3 inverseDiffuse = inverseDiff*lightDiffuse*materialDiffuse;
-	
-	//result
-	//vec3 result = (ambient+diffuse+specular)*texture(materialColors,vUv0).rgb;
-	
-	vec3 color = color0 * color1 * FB * intLuzAmb;
-	if(gl_FrontFacing){
-        fFragColor = OutColor * vec4(color, 1.0) * FB * vec4(diffuse,1.0);
-    }else{
-        fFragColor = InColor * vec4(color, 1.0) * FB * vec4(inverseDiffuse,1.0);
-    }
-    if(color1.x > 0.6){
+	if(color1.x > 0.6){
         discard;
     }
 	
+	//diffuse
+	vec3 norm = normalize(vWnormal);
 	
+	if(!gl_FrontFacing)
+        norm = -norm;
+		
+	
+	vec3 lightDir = lightPosition;
+	float diff = max(dot(norm,lightDir),0.0);
+	vec3 diffuse = diff*lightDiffuse*materialDiffuse;
+	
+	if(gl_FrontFacing){
+		fFragColor = OutColor * vec4(color0, 1.0) * vec4(diffuse,1.0);
+    }else{
+		fFragColor = InColor * vec4(diffuse,1.0);
+    }
 }
--- media/IG2App/Ejercicio2bVS.glsl
+++ media/IG2App/Ejercicio2bVS.glsl
@@ -6,13 +6,15 @@ in vec2 uv0;
 out vec3 vWvertex; //en world space
 out vec3 vWnormal; //en world space
 out vec2 vUv0;
-uniform mat4model;
-uniform mat4view;
-uniform mat4projection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 normalMat;
 
 void main() {
-	vWvertex = vec3(model*vertex); // en world space
-	vWnormal = mat3(transpose(inverse(model)))*normal;
 	vUv0 = uv0;
+	vWvertex = vec3(model*vertex); // en world space
+	vWnormal = mat3(normalMat)*normal;
+	
 	gl_Position = projection*view*vec4(vWvertex, 1.0);
 }
